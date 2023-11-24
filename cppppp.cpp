c7d7722d13016a33e6f528f4d798fb0be7e63f1f
zurnadurumm <kaanc645@gmail.com>
2023-11-24 19:38:20 +0400

    final commit

--- cppppp.cpp
+++ cppppp.cpp
@@ -1,7 +1,19 @@
+#ifdef _WIN32
+	#define CLEAR "cls"
+	#define PAUSE_COMMAND "pause"
+#else 
+	#define CLEAR "clear"
+	#define PAUSE_COMMAND "read -p 'Press Enter to continue...' -n 1 -r"
+#endif
+
 #include<iostream>
 #include<fstream>
 #include<string>
 #include<cstdlib>
+
+
+
+
 
 using namespace std;
 
@@ -26,6 +38,7 @@ public:
 };
 
 
+fstream file;
 //Add func
 void Dict::sadd() {
 	cout << "****** ADD NEW WORDS ******\n" <<
@@ -33,24 +46,114 @@ void Dict::sadd() {
 	cout << "EN: "; cin >> word.en;
 	cout << "AZ: "; cin >> word.az;
 
-	fstream file;
 
 	file.open("file.txt", ios::app);
 	file << word.en << endl << word.az << endl;
 	file.close();
-	system("pause");
-	system("CLS");
+	system(PAUSE_COMMAND);
+	system(CLEAR);
 
 }
 
 //List func
-void Dict::slist(){}
+void Dict::slist(){
+	cout << "****** LIST OF WORDS ******\n";
+		file.open("file.txt", ios::in);
+		while (file >> word.en >> word.az)
+		{
+			cout << "EN: " << word.en << endl
+			<< "AZ: " << word.az << endl
+			<< "==========================" << endl;
+		}
+		file.close();
+		system(PAUSE_COMMAND);
+		system(CLEAR);
+		
+		
+}
 
 //Sort func
-void Dict::ssort(){}
+void Dict::ssort(){
+	cout << "****** SORTED LIST ******\n";
+	file.open("file.txt", ios::in);
+	string array[100];
+	int i = 0;
+	while (file >> word.en >> word.az)
+	{
+		array[i] = word.en;
+		i++;
+	}
+	file.close();
+	int n = i;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++) {
+			if (array[i] > array[j]) {
+				string temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		file.open("file.txt", ios::in);
+		while (file >> word.en >> word.az)
+		{
+			if (array[i] == word.en)
+			{
+				cout << "EN: " << word.en << endl
+					<< "AZ: " << word.az << endl
+					<< "==========================" << endl;
+			}
+		}
+		file.close();
+	}
+	system(PAUSE_COMMAND);
+	system(CLEAR);
+
+}
 
 //Delete func
-void Dict::sdel(){}
+void Dict::sdel(){
+	cout << "****** DELETE WORD ******\n" <<
+		"Enter word EN\n";
+	string s;
+	cout << "EN: "; cin >> s;
+
+	file.open("file.txt", ios::in);
+	int k = 0;
+	while (file >> word.en >> word.az)
+	{
+		if (s.compare(word.en) == 0)
+		{
+			k = 1;
+			break;
+		}
+	}
+	file.close();
+	if (k == 0) cout << "Not Found\n";
+	else {
+		file.open("file.txt", ios::in);
+		ofstream temp;
+		temp.open("temp.txt", ios::out);
+		while (file >> word.en >> word.az)
+		{
+			if (s.compare(word.en) != 0)
+			{
+				temp << word.en << endl << word.az << endl;
+			}
+		}
+		file.close();
+		temp.close();
+		remove("file.txt");
+		rename("temp.txt", "file.txt");
+		cout << "Deleted\n";
+	}
+	system(PAUSE_COMMAND);
+	system(CLEAR);
+	
+}
 
 //Quit func
 void Dict::squit(){
@@ -64,7 +167,6 @@ void Dict::sfind(){
 	string s;
 	cout << "EN: "; cin >> s;
 
-	fstream file;
 
 	file.open("file.txt", ios::in);
 	int k = 0;
@@ -80,8 +182,8 @@ void Dict::sfind(){
 	file.close();
 	if (k == 0) cout << "Not Found\n";
 
-	system("pause");
-	system("CLS");
+	system(PAUSE_COMMAND);
+	system(CLEAR);
 
 
 }
@@ -100,34 +202,37 @@ void Dict::menu(){
 		int n;
 		cin >> n;
 
+
+
 		switch (n)
 		{
 		case 1:
-			system("CLS");
+			system(CLEAR);
 			sadd();
 			break;
 		case 2:
-			system("CLS");
+			system(CLEAR);
 			sfind();
 			break;
 		case 3:
-			system("CLS");
+			system(CLEAR);
 			slist();
 			break;
 		case 4:
-			system("CLS");
+			system(CLEAR);
 			ssort();
 			break;
 		case 5:
-			system("CLS");
+			system(CLEAR);
 			sdel();
 			break;
 		case 6:
 			squit();
 			break;
 		default:
+
 			cout << " Invalid character\n";
-			system("CLS");
+			system(CLEAR);
 			break;
 		}
 
